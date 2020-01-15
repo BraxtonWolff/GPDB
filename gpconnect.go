18b7bedb3e0aae643e3a3851deb44a2c546049ac
Liu <fishiskilled@163.com>
2020-01-15 17:05:15 +0800

    gpconnect

---
+++ gpconnect.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	host     string = "172.36.21.200"
+	port     int    = 5432
+	user     string = "gpadmin"
+	passworf string = "gpadmin"
+	dbname   string = "db01"
+)
+
+func gpconnect() {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, passworf, dbname)
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("链接成功")
+}
+
+var name string
+
+func main() {
+	var age int
+	x := 100
+	name = "zhangsan"
+	age = 18
+	fmt.Println("hello world")
+	fmt.Println(x)
+	fmt.Println(name)
+	fmt.Println(age)
+}
